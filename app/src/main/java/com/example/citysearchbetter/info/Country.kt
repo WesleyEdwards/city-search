57a39eaef72c5c325c460681b6303f1b090efa21
Wesley Edwards <a02267634@aggies.usu.edu>
2022-05-13 05:20:28 -0600

    Adding country data classes

---
+++ app/src/main/java/com/example/citysearchbetter/info/Country.kt
@@ -0,0 +1,29 @@
+package com.example.citysearchbetter.info
+
+import com.google.gson.annotations.SerializedName
+
+data class Country(
+    @SerializedName("geonameid") val geonameId: Long,
+    val name: String,
+    val code: String,
+    val capital: Capital,
+    @SerializedName("area_size") val areaSize: String,
+    val population: Long,
+    val flag: Flag,
+    @SerializedName("wiki_url") val wikiUrl: String,
+    val currency: Currency,
+)
+
+data class Capital(
+    val name: String,
+    val geonameId: Long
+)
+
+data class Flag(
+    val file: String
+)
+
+data class Currency(
+    val code: String,
+    val name: String
+)
